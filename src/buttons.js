40591ddf62bc2c510ae4c6f7ab33e0f28b15aa97
Michael Chan <mijoch@gmail.com>
2018-07-31 17:19:13 -0700

    setup project structure and deps

---
+++ src/buttons.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Button from "react-native-web";
+
+export default function(props) {
+  return <Button {...props} />;
+}
---
+++ src/typography.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Text from "react-native-web";
+
+export default function(props) {
+  return <Text {...props} />;
+}
